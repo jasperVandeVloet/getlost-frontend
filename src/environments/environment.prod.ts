02ecb7dd041bc9f6a3ee82250663cf7a80239bd5
jasper VdV <jaspervandevloet@gmail.com>
2020-05-11 15:31:01 +0200

    fix frontendpath

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBasePath: 'https://getlost-backend.herokuapp.com'
+  apiBasePath: 'https://getlost-backend.herokuapp.com/',
+  frontendBasePath: 'https://getlost-frontend.netlify.app/'
 };
