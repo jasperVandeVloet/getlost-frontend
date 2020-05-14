d0f2e9ba410d7027bc8f736ac3c5425de06043ee
jasper VdV <jaspervandevloet@gmail.com>
2020-05-14 12:19:35 +0200

    navbar redesign

--- src/main.ts
+++ src/main.ts
@@ -29,5 +29,5 @@ platformBrowserDynamic()
       path: '/assets/json/loading.json'
     });
   })
-  .then(() => setTimeout(() => loadingElement.remove(), 4000))
+  .then(() => setTimeout(() => loadingElement.remove(), 2000))
   .catch(err => console.error(err));
