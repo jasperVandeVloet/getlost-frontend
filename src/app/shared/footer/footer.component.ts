45fa0b5c1da75e80c080d5cd6da78843303c9352
jasper VdV <jaspervandevloet@gmail.com>
2020-06-13 15:59:35 +0200

    footer logo animation

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -4,6 +4,9 @@ import {  Router } from '@angular/router';
 import { environment } from '../../../environments/environment';
 import { HelperService } from 'src/app/service/helper.service';
 
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -12,6 +15,12 @@ import { HelperService } from 'src/app/service/helper.service';
 })
 export class FooterComponent implements OnInit {
   public path = environment.frontendVariablePath;
+  public options: AnimationOptions = {
+    // path: 'https://assets3.lottiefiles.com/packages/lf20_ZWEJL5.json',
+    path: '/assets/json/logo-spinning.json',
+    autoplay: true,
+    loop: true
+  };
 
   constructor(
     private router: Router,
